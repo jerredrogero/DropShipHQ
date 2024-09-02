58914db0a75c773d21528e0000040b3d346cc51b
djrdog578 <jrogero578@gmail.com>
2024-09-02 19:53:09 -0400

    Fix: Corrected DJANGO_SETTINGS_MODULE in wsgi.py

--- dropshiphq/wsgi.py
+++ dropshiphq/wsgi.py
@@ -16,6 +16,6 @@ sys.path.insert(0, os.path.dirname(PROJECT_ROOT))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dropshiphq.settings')
 
 application = get_wsgi_application()
