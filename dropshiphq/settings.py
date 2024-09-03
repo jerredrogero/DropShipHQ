fd2b62401a13f737fdd8c530ae06a76223e2dbd2
djrdog578 <jrogero578@gmail.com>
2024-09-03 13:04:27 -0400

    converting to postgres

--- dropshiphq/settings.py
+++ dropshiphq/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,10 +79,7 @@ WSGI_APPLICATION = 'dropshiphq.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
