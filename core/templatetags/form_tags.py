3dec3a5e2d306ea656f7917407931f3f911acbd5
djrdog578 <jrogero578@gmail.com>
2024-10-30 19:00:36 -0400

    Fixed date bug and updated features

--- core/templatetags/form_tags.py
+++ core/templatetags/form_tags.py
@@ -11,3 +11,7 @@ def add_class(value, arg):
     else:
         # This is likely a string, just return it unchanged
         return value
+
+@register.filter(is_safe=True)
+def safe_link(value):
+    return f"<a href='https://www.amazon.com/gp/your-account/order-details?orderID={value}' target='_blank'>{value}</a>"
